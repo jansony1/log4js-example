b5fcf435248314bce344481713b74d5b2621d409
jansony1 <ncatyu@126.com>
2018-07-14 17:45:34 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,69 +1,107 @@
-var log4js = require('log4js');
-var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var AWS = require("aws-sdk");
+var fs = require('fs');
+const inquirer = require("inquirer");
+const log4js = require('log4js');
 
-//We won't need this.
-//var logger = require('morgan');
-var log = log4js.getLogger("app");
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(favicon());
-
-// replace this with the log4js connect-logger
-// app.use(logger('dev'));
-app.use(log4js.connectLogger(log4js.getLogger("http"), { level: 'auto' }));
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+log4js.configure({
+  appenders: { cheese: { type: 'file', filename: 'result.log' } },
+  categories: { default: { appenders: ['cheese'], level: 'info' } }
 });
 
-/// error handlers
+const logger = log4js.getLogger('cheese');
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        log.error("Something went wrong:", err);
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+
+AWS.config.update({
+    region: "us-west-2"
+});
+
+
+
+var docClient  = new AWS.DynamoDB.DocumentClient();
+
+//docClient.setMaxListeners(0);
+
+console.log("Importing movies into DynamoDB. Please wait.");
+
+
+//question list on shell
+var questions = [
+ {
+    type: "input",
+    name: "concurrency",
+    message: "please input desired test concurrency :",
+    default: 100
+  },
+  {
+    type: "input",
+    name: "timer",
+    message: "please input desired test time by Seconds :",
+    default: 50000
+  },
+
+]
+
+//设置命令行交互
+inquirer
+  .prompt(questions)
+  .then(answers => {
+      var concurrency = answers.concurrency   
+
+      var timer = answers.timer  
+
+      //设置每秒并发
+      var myInterval=setInterval(sendDataConcurrent,1000, concurrency);
+      //设置执行时间，执行时间过后会stop掉上面的并发
+      setTimeout(()=>{
+        clearTimeout(myInterval);
+      }, timer) 
+});
+
+
+
+
+// 模拟一次api网络请求花费1s
+async function _a_ddb_put(result) {
+  return new Promise((resolve, reject) => {
+
+
+  	var params = {
+        TableName: "Movies",
+        Item: {
+            "year":  result,
+            "title": result+"121",
+            "info":  result+"222",
+        }
+    };
+
+    docClient.put(params, function(err, data) {
+       if (err) {
+           logger.error("Unable to add movie", result, ". Error JSON:", JSON.stringify(err, null, 2))
+           console.error("Unable to add movie", result, ". Error JSON:", JSON.stringify(err, null, 2));
+           reject(err);
+       } else {
+       	   
+           logger.info("PutItem succeeded:", result, " #######time is :", new Date().getMinutes(), ":",new Date().getSeconds())
+           console.log("PutItem succeeded:", result, " #######time is :", new Date().getMinutes(), ":",new Date().getSeconds());
+		       resolve();
+       }
     });
+   	
+  });
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    log.error("Something went wrong:", err);
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+async function sendDataConcurrent(concurrency) {
 
 
-module.exports = app;
+  const t1 = Date.now();
+  const promises = [];
+  for (let i = 1; i <= concurrency; i += 1) {
+    promises.push(_a_ddb_put(i));
+  }
+  const result = await Promise.all(promises)
+  const t2 = Date.now();
+  console.log(`total cost: ${t2 - t1}ms.`)
+  logger.info(`total cost: ${t2 - t1}ms.`)
+  return result;
+}
